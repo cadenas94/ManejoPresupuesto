b250fc6d976cce256fd6b31252b9d341a912fa8b
DanielCadenas <danicadenascab@gmail.com>
2023-06-19 22:58:46 +0200

    Validaciones por atributo y modelo - Insertando datos en la BBDD

---
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data.Common;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController: Controller
+    {
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+
+        public TiposCuentasController(IRepositorioTiposCuentas repositorioTiposCuentas) 
+        {
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+        }
+        public IActionResult Crear()
+        {
+                return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear (TipoCuenta tipoCuenta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+
+            tipoCuenta.UsuarioId = 1;
+            repositorioTiposCuentas.CrearTipoCuenta(tipoCuenta);
+
+            return View();
+        }
+    }
+}
---
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,41 @@
+﻿using ManejoPresupuesto.Validaciones;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.PortableExecutable;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta //: IValidatableObject
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        //[PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+
+        /*Pruebas de otras validaciones por defecto*/
+        //[Required(ErrorMessage = "El campo {0} es requerido")]
+        //[EmailAddress(ErrorMessage = "El campo debe ser un correo electrónico válido")]
+        //public string Email { get; set; }
+        //[Range(minimum: 18, maximum: 130, ErrorMessage = "El valor debe estar entre {1} y {2}")]
+        //public int Edad { get; set; }
+        //[Url(ErrorMessage = "El campo debe ser una URL valida")]
+        //public string URL  { get; set; }
+        //[CreditCard(ErrorMessage = "La tarjeta de credito noe s valida")]
+        //[Display(Name = "Tarjetilla de credito")]
+        //public string TarjetaDeCredito{ get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Nombre != null && Nombre.Length > 0)
+        //    {
+        //        var primeraLetra = Nombre[0].ToString();
+
+        //        if(primeraLetra != primeraLetra.ToUpper())
+        //        {
+        //            yield return new ValidationResult("La primera letra debe ser mayúsculaa", new[] { nameof(Nombre) });
+        //        }
+        //    }
+        //}
+    }
+}
