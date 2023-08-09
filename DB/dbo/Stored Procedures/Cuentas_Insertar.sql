922ed5b1d11959fcd169b4ce134eb872b607988b
DanielCadenas <danicadenascab@gmail.com>
2023-08-09 13:43:57 +0200

    Adding DB

---
+++ DB/dbo/Stored Procedures/Cuentas_Insertar.sql
@@ -0,0 +1,30 @@
+﻿
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[Cuentas_Insertar]
+	@Nombre nvarchar(50),
+	@TipoCuentaId int,
+	@Balance Decimal(18,2),
+	@Descripcion nvarchar(250)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    DECLARE @Orden int;
+	
+	--SELECT @Orden = COALESCE(MAX(Orden), 0) +1
+	--FROM TiposCuentas
+	--WHERE UsuarioId = @UsuarioId
+
+	INSERT INTO Cuentas(Nombre, TipoCuentaId, Balance, Descripcion)
+	VALUES(@Nombre, @TipoCuentaId, @Balance, @Descripcion);
+
+	SELECT SCOPE_IDENTITY();
+
+
+END
---
+++ DB/dbo/Tables/TEST.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TEST] (
+    [Entity]   NCHAR (100)     NOT NULL,
+    [Proyecto] NCHAR (100)     NOT NULL,
+    [Ceco]     NCHAR (100)     NOT NULL,
+    [Time]     NCHAR (100)     NOT NULL,
+    [HC_FTE]   NCHAR (100)     NOT NULL,
+    [Amount]   DECIMAL (18, 2) NOT NULL
+);
+
